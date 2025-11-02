ff7dcef00fa888b71f55906e6e8598c9bb7cf0db
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-11-02 14:07:00 +0200

    Programmatic Navigation with useNavigate

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -11,20 +11,26 @@ const formatDate = (date) =>
 
 function City() {
   const {id} = useParams();
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
 
-  const { cityName, emoji, date, notes } = currentCity;
+  const [searchParams, setSearchParams] = useSearchParams();
 
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
   
+  // TEMP DATA
+  // const currentCity = {
+  //   cityName: "Lisbon",
+  //   emoji: "🇵🇹",
+  //   date: "2027-10-31T15:59:59.138Z",
+  //   notes: "My favorite city so far!",
+  // };
 
-  return <h1>City {id}</h1>;
+  // const { cityName, emoji, date, notes } = currentCity;
 
+  return <>
+        <h1>City {id}</h1>
+      <p>{lat}, {lng}</p>
+  </>;
   // return (
   //   <div className={styles.city}>
   //     <div className={styles.row}>
