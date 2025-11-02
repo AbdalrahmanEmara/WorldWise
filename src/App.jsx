5fa68301eb4973e913358182d6ddb2e73b8a3add
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-11-02 12:49:34 +0200

    Dynamic Routes With URL Parameters

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
@@ -8,6 +9,7 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import City from './components/City';
 
 const BASE_URL = "http://localhost:9000";
 
@@ -42,6 +44,7 @@ export default function App() {
         <Route path='app' element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path='cities/:id' element={<City />} />
           <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path='form' element={<p>Form</p>} />
         </Route>
