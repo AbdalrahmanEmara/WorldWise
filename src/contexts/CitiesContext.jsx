20d4db853c227cec63f91177cd2a9f67e81de3a0
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-11-11 23:39:30 +0200

    Deleting a city Delete from Fake API update cities state

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -52,7 +52,22 @@ function CitiesProvider({ children }) {
       const data = await res.json();
       setCities(cities => [...cities, data]);
     } catch {
-      console.error("There was an error loading data...");
+      console.error("There was an error creating city...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+    async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+      });
+
+      setCities(cities => cities.filter(city => city.id !== id));
+    } catch {
+      console.error("There was an error deleting city...");
     } finally {
       setIsLoading(false);
     }
@@ -67,6 +82,7 @@ function CitiesProvider({ children }) {
         currentCity,
         getCity,
         createCity,
+        deleteCity,
       }}>
       {children}
     </CitiesContext.Provider>
